736c69df1c9ba80b7a1623beabafe9744aa0b876
Darius Ghomashchian <Darius0852@yahoo.co.uk>
2021-03-15 23:55:27 +0000

    Initial
    Update - Initial Commit

---
+++ CGP - Lab 2/Assets/scripts/buttonGlow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class buttonGlow : MonoBehaviour
+{
+    MeshRenderer mr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Behaviour halo = (Behaviour)GetComponent("Halo");
+
+        halo.enabled = true; // false
+    }
+}
